5d96604f4c4ece4d48d9febcdba8fbe238dda6c0
Akshay-Yadav-Atgeir <133030397+Akshay-Yadav-Atgeir@users.noreply.github.com>
2023-06-22 19:20:22 +0530

    Update streamlit_app.py
    update text and "

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,10 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.title('Breakfast Menu')
+streamlit.header('Breakfast Menu')
 
-streamlit.write("Omega 3 & Blueberry Oatmeal")
+streamlit.text('Omega 3 & Blueberry Oatmeal')
 
-streamlit.write("Kale, Spinach & Rocket Smoothie")
+streamlit.text('Kale, Spinach & Rocket Smoothie')
 
-streamlit.write("Hard-Boiled Free-Range Egg")
+streamlit.text('Hard-Boiled Free-Range Egg')
