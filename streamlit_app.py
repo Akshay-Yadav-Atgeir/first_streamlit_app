224cc4df72af2a09e278884b72cc8d2ab179b817
Akshay-Yadav-Atgeir <133030397+Akshay-Yadav-Atgeir@users.noreply.github.com>
2023-06-22 19:17:50 +0530

    Update streamlit_app.py
    added breakfast details

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ streamlit.title('Breakfast Menu')
 streamlit.write("Omega 3 & Blueberry Oatmeal")
 
 streamlit.write("Kale, Spinach & Rocket Smoothie")
+
+streamlit.write("Hard-Boiled Free-Range Egg")
